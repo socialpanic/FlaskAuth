b6ba2526de2777e86bfe78bac6411694dc95c39f
Socialpanic <mitchell.r.adams@gmail.com>
2015-04-12 03:51:20 +0000

    basic create view and delete of student objects. authentication and registration seems solid

--- app/views.py
+++ app/views.py
@@ -8,22 +8,38 @@ import time
 import datetime
 import random
 import itertools
+import json
 
 #Note: This is actually a stripped down product from a larger app
 #I built and not all of the above imports are necessary to run this
 #app but I am lazy and left them.
 
-data = []
+msg=''
+data = {}
 '''
 be sure to edit this information with the creditentials for YOUR sql server
 '''
-mysql2 = MySQLdb.connect(host= "mysql.server",
-    user="wvup",
-    passwd="pass",
-    db="wvup$hawkcrypt")
+def dbconnect():
+    mysql2 = MySQLdb.connect(host= "mysql.server",
+        user="wvup",
+        passwd="pass",
+        db="wvup$hawkcrypt")
+    return mysql2
+
 '''
+
 Did you change the mysql connect information to match YOUR mysql server????
 '''
+def read_students():
+
+    db = dbconnect()
+    conn = db.cursor()
+    conn.execute("SELECT * FROM Students")
+    data = conn.fetchall()
+    data=list(data)
+    return data
+
+
 
 '''
 CHANGE UPLOAD_FOLDER PATH
@@ -44,16 +60,21 @@ def index():
 @app.route('/home',  methods = ['GET', 'POST'])
 def home():
         logged=True
+        data.clear()
+        studentdata = read_students()
+        data["students"] = studentdata
+
         if os.path.isfile(UPLOAD_FOLDER+session['username']+"pic") == True:
             profilepic = str(session['username']+"pic")
-            return render_template ("member.html", logged=logged, profilepic=profilepic, session=session)
+            return render_template ("member.html", logged=logged, profilepic=profilepic, session=session, data=data)
         else:
-            return render_template ("member.html", logged=logged, session=session)
+            return render_template ("member.html", logged=logged, session=session, data=data)
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
 	msg = ""
-	conn = mysql2.cursor()
+	db = dbconnect()
+	conn = db.cursor()
 	session["username"] = request.form["user"]
 	session["password"] = request.form["pass"]
 	conn.execute("SELECT * from Users where username='" + session["username"] + "' and password='" + session["password"] + "'")
@@ -93,8 +114,8 @@ def editprofile():
 
 @app.route("/join", methods = ['GET', 'POST'])
 def join():
-
-    conn = mysql2.cursor()
+    db = dbconnect()
+    conn = db.cursor()
     firstname = request.form["firstname"]
     lastname = request.form["lastname"]
     username = request.form["username"]
@@ -106,25 +127,26 @@ def join():
 		conn.execute("SELECT * FROM Users WHERE username='" + username + "' OR email='" + email + "'")
 		data = conn.fetchone()
 		if data is None:
-			path = os.curdir
-			conn.execute("INSERT INTO Users (UID, firstname, lastname, email, password, username) VALUES ('DEFAULT', '"+firstname+"', '"+lastname+"', '"+email+"', '"+password+"', '"+username+"')")
-			logged = False
-			MySQLdb.connection.commit(mysql2)
-			return render_template ('index.html', msg=msg, logged=logged)
+		    msg = "Creating Account. Please try logging in."
+		    path = os.curdir
+		    conn.execute("INSERT INTO Users (UID, firstname, lastname, email, password, username) VALUES ('DEFAULT', '"+firstname+"', '"+lastname+"', '"+email+"', '"+password+"', '"+username+"')")
+		    logged = False
+		    MySQLdb.connection.commit(db)
+		    return render_template ('index.html', msg=msg, logged=logged)
 		else:
 		    logged = False
-		    msg = "That username or password already exists"
+		    msg = "An account with that username or email already exists"
 		    return render_template ('index.html', msg=msg, logged=logged)
     else:
         msg = "The passwords do not match. Please try again."
-        return render_template ('index.html', msg=msg, logged=logged)
+        return render_template ('index.html', msg=msg)
 
 @app.route('/update_profile', methods=['GET','POST'])
 def update_profile():
     if session["logged_in"] == True:
         username = session['username']
 
-        conn = mysql2.cursor()
+        conn = dbconnect()
         setstring = ""
 
         firstname = request.form["firstname"]
@@ -176,7 +198,7 @@ def upload_file():
     	if request.method == 'POST':
     		file = request.files['file']
 
-	        conn = mysql2.cursor()
+	        conn = dbconnect()
 	        conn.execute("SELECT * FROM Users WHERE username = '"+str(session['username'])+"'")
 	        MySQLdb.connection.commit(mysql2)
 	        data = conn.fetchone()
@@ -190,3 +212,66 @@ def upload_file():
     		    conn.execute("UPDATE Users SET profilepic = '"+profilepic+"' WHERE username = '"+str(session['username'])+"'")
     		    MySQLdb.connection.commit(mysql2)
     		    return redirect(url_for('home'))
+
+@app.route('/create_student', methods=['GET', 'POST'])
+def create_student():
+    db = dbconnect()
+    conn = db.cursor()
+
+    fname = request.form["new_student_fname"]
+    lname = request.form["new_student_lname"]
+    age = request.form["new_student_age"]
+    birthmonth = request.form["new_student_dob_month"]
+    birthday = request.form["new_student_dob_day"]
+    birthyear = request.form["new_student_birthyear"]
+    phone = request.form["new_student_phone"]
+    email = request.form["new_student_email"]
+    address1 = request.form["new_student_address1"]
+    address2 = request.form["new_student_address2"]
+    city = request.form["new_student_city"]
+    state = request.form["new_student_state"]
+    zipcode = request.form["new_student_zip"]
+    major = request.form["new_student_major"]
+    advisor = request.form["new_student_advisor"]
+    gpa = request.form["new_student_gpa"]
+    password = request.form["new_student_password"]
+    confirmpass = request.form["confirmpass"]
+    credithours = request.form["new_student_credits"]
+    licenseplate = request.form["new_student_license"]
+
+
+
+    if password == confirmpass:
+		conn.execute("SELECT * FROM Students WHERE email='"+email+"'")
+		data = conn.fetchone()
+		if data is None:
+		    logged = True
+		    msg = "Student Added to Database"
+		    conn.execute("INSERT INTO Students (sid, fname, lname, age, birth_month, birth_day, birth_year, phone, email, address1, address2, city, state, zip, major, advisor, gpa, password, credit_hours, license_plate) VALUES ('DEFAULT', '"+fname+"', '"+lname+"', '"+age+"', '"+birthmonth+"', '"+birthday+"', '"+birthyear+"', '"+phone+"','"+email+"','"+address1+"','"+address2+"','"+city+"','"+state+"','"+zipcode+"','"+major+"','"+advisor+"','"+gpa+"','"+password+"','"+credithours+"','"+licenseplate+"')")
+		    MySQLdb.connection.commit(db)
+		    return redirect(url_for('home'))
+		else:
+		    logged = False
+		    msg = "A student with that email address already exists"
+		    return redirect(url_for('home'))
+    else:
+      msg = "Logged Out. Please Try Again"
+      return render_template ('index.html', msg=msg)
+
+@app.route('/delete_student', methods=['GET', 'POST'])
+def delete_student(x):
+    return x
+
+@app.route('/delete/<x>', methods = ['GET', 'POST'])
+def answer(x):
+    db = dbconnect()
+    conn = db.cursor()
+    query = "DELETE FROM Students WHERE sid = "+x+";"
+
+    conn.execute(query)
+    MySQLdb.connection.commit(db)
+    return redirect(url_for('home'))
+
+
+
+
